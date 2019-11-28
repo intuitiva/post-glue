8b441e44499c809e7e295dbbe2dfb46c7365e09b
Luis Pedro Sarti <sartip@intuitiva.biz>
2019-11-28 09:48:02 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Proceso Post-Glue (Notificar a Zauru que Glue terminó):
+
+Este proyecto consiste en configurar CloudWatch para que envíe un evento a esta función lambda que enviará la notificación a Zauru para ver si dio error o fue satisfactorio el Glue Job.
+
+## Deploy con Serverless Framework
+1. npm install
+2. configurar .env (ejemplo en el siguiente punto)
+2. serverless deploy
+
+> Gracias al Serverless Framework, en donde los permisos IAM se definen en el código, no hay que configurar nada para poder subir el código. Solo tenemos que asegurarnos que el .env esté correcto (siguiente punto)
+
+### Variables de entorno a configurar en el archivo .env
+```
+AWS_S3_ACCESS_KEY=SECRET1
+AWS_S3_SECRET_KEY=SECRET2
+AWS_AZ_REGION=us-east-1
+AWS_S3_BUCKET_DESTINATION=destination_bucket
+ZAURU_GET_UUID_URL_PREFIX=https://app.zauru.com/data_import_jobs/
+ZAURU_GET_UUID_URL_SUFIX=/get_uuid_based_on_external_id.json
+ZAURU_PUT_URL=https://app.zauru.com/data_import_jobs/
+ZAURU_HEADER_USER_EMAIL=pruebas@zauru.com
+ZAURU_HEADER_USER_TOKEN=SOsdfpSPDSpsosFUFJ
+```
+
+### Pruebas desde el Serverless Framework
+
+Se colocó un evento de prueba en el código con el archivo ``event.json`` ese archivo se puede utilizar para probar el evento de la siguiente forma:
+
+```
+serverless invoke local --function cloudwatchglues3cleaner --path event.json
+```
+
+## Configuración de AWS Cloudwatch para que haga trigger esta función
+
+1. Entrar a CloudWatch
+2. Entrar a propiedades del bucket
+3. Entrar a eventos y seleccionar "+ Añadir notificación"
+4. Seleccionar los eventos PUT y POST
+5. En "Enviar a" seleccionar "Función de Lambda"
+6. Seleccionar en "Lambda" esta función.
+
+Licencia MIT
---
+++ handler.js
@@ -0,0 +1,91 @@
+'use strict';
+
+//Call Libraries
+//const AWS = require('aws-sdk');
+const axios = require('axios')
+
+//Define AWS resources objects
+//var s3 = new AWS.S3();
+
+//Lambda handler for the S3 File Processing
+module.exports.cloudwatchglues3cleaner = (event, context, callback) => {
+
+  let myProm = new Promise(async function(resolve, reject){
+
+    console.log("Event ==> " + JSON.stringify(event))
+
+    //Set the Headers for all Zauru REST API Requests
+    var headersdata = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-User-Email': process.env.ZAURU_HEADER_USER_EMAIL,
+      'X-User-Token': process.env.ZAURU_HEADER_USER_TOKEN
+    };
+
+    // Get glue job event information
+    if (event.hasOwnProperty('detail')) {
+      var glue_job_id = event.detail.jobRunId;
+      var glue_job_state = event.detail.state;
+      var glue_job_message = event.detail.message;
+
+      if (glue_job_state == 'SUCCEEDED') {
+        var is_error = false;
+      } else {
+        var is_error = true;
+      }
+
+      var get_options = {
+        headers: headersdata,
+        url: process.env.ZAURU_GET_UUID_URL_PREFIX + glue_job_id + process.env.ZAURU_GET_UUID_URL_SUFIX,
+        method: 'GET'
+      }
+
+      axios(get_options).then(function (response) {
+
+        console.log("GET Zauru API success! " + response.data);
+        var id = response.data;
+
+        // Configure call PUT Zauru API
+        var put_data = {
+          "data_import_job": 
+            {
+              "ready_to_fetch_to_local_db": !is_error,
+              "source": "Post-Glue lambda", 
+              "status": glue_job_state + " > " + glue_job_message, 
+              "percentage_completed": "60",
+              "is_error": is_error
+            }
+        };
+
+        var put_options = {
+          data: put_data,
+          headers: headersdata,
+          url: process.env.ZAURU_PUT_URL + id + ".json",
+          method: "PUT"
+        };
+  
+        // Really call PUT Zauru API
+        axios(put_options).then(function (response) {
+  
+          console.log("PUT Zuru API success!")
+          callback(null, "Successfully PUT!")
+        }) // in case the PUT didnt work
+          .catch(function (error) { // If error
+            console.log(error);
+            callback(null, "Error: PUT didnt work")
+          });
+
+      }) // in case the GET didnt work
+        .catch(function (error) { // if error on GET
+          console.log(error);
+          callback(null, "Error: GET didnt work")
+        });
+
+    } // in case the event param doesnt have "detail"
+    else {
+      callback(null, "Event doesnt have detail")
+    }
+
+  });
+};
+
